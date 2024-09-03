0c02b20cde659147bd76ec854c3bf75503f5cf89
Asif-Ali-1 <asif7171786@gmail.com>
2024-09-03 18:05:21 +0500

    dsf

--- src/Component/Pages.js
+++ src/Component/Pages.js
@@ -98,7 +98,7 @@ function Pages() {
                 <p className="card-text"> <small className="text-body-secondary">{product.price}</small></p>
               </div>
                 <div class="card-footer">
-                <a href="#" className="btn btn-primary">
+                <a href="#" className="btn btn-danger">
                 Add to cart</a>
                 </div>
             </div>
